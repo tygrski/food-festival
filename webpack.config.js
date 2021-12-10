fdc5ff3c6282ba021b45eda72e9049dbe2315e14
tygrski <77369211+tygrski@users.noreply.github.com>
2021-12-10 16:24:54 -0600

    webpack-img-optimized

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,30 @@ module.exports = {
    filename: '[name].bundle.js',
    path: __dirname + '/dist',
   },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+          loader: 'file-loader',
+          options: {
+            esModule: false,
+            name (file) {
+              return "[path][name].[ext]"
+            },
+            publicPath: function(url) {
+              return url.replace("../", '/assets/')
+            }
+          }
+          },
+          {
+            loader: 'image-webpack-loader'
+          }
+        ]
+      }
+    ]
+  },
   
   // let webpack know to use JQuery
   plugins: [
